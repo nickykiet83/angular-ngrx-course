a7fd429465656882f100e55f89ec54d1a61450d4
Kiệt Phạm Quang Anh <phamquanganhkiet@gmail.com>
2019-01-16 19:22:07 +1100

    Speed Up Reducer Development Using Ngrx Schematics

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -4,38 +4,12 @@ import { ActionReducerMap, MetaReducer } from '@ngrx/store';
 
 import { environment } from '../../environments/environment';
 
-export interface AuthState {
-    loggedIn: boolean;
-    user: User;
-}
 
 export interface AppState {
-    auth: AuthState;
 }
 
-const initialAuthState: AuthState = {
-    loggedIn: false,
-    user: undefined,
-};
-
-
-function authReducer(state: AuthState = initialAuthState, action): AuthState {
-    switch (action.type) {
-        case AuthActionTypes.LoginAction:
-            return {
-                loggedIn: true,
-                user: action.payload.user
-            };
-        case AuthActionTypes.LogoutAction:
-            break;
-        default:
-            return state;
-    }
-    return state;
-}
 
 export const reducers: ActionReducerMap<AppState> = {
-    auth: authReducer
 };
 
 
