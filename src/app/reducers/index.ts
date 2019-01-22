13c98871de29ab1a3123d5cdf9cda7b7e5a7e86a
Kiệt Phạm Quang Anh <phamquanganhkiet@gmail.com>
2019-01-22 12:24:44 +1100

    format code

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -1,27 +1,17 @@
-import {
-  ActionReducer,
-  ActionReducerMap,
-  createFeatureSelector,
-  createSelector,
-  MetaReducer
-} from '@ngrx/store';
+import { routerReducer, RouterReducerState } from '@ngrx/router-store';
+import { ActionReducerMap, MetaReducer } from '@ngrx/store';
+import { storeFreeze } from 'ngrx-store-freeze';
+
 import { environment } from '../../environments/environment';
-import {User} from '../model/user.model';
-import {AuthActions, AuthActionTypes} from '../auth/auth.actions';
-import {storeFreeze} from 'ngrx-store-freeze';
-import {routerReducer} from '@ngrx/router-store';
 
 
 export interface AppState {
-
+    router: RouterReducerState;
 }
 
 export const reducers: ActionReducerMap<AppState> = {
   router: routerReducer
 };
-
-
-
 
 
 export const metaReducers: MetaReducer<AppState>[] =
